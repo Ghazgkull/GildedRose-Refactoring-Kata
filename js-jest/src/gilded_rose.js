f3fc585f61001da1c7d81c70265f68d76cd8c38f
Jared Burns <jburns@Jareds-MacBook-Pro.local>
2022-02-18 12:25:15 -0800

    Refactoring initial pass

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -1,5 +1,5 @@
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -7,61 +7,81 @@ class Item {
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
+    this.qualityChange = -1;
   }
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
-
+    this.items.forEach((item) => {
+      this.adjustSellInDate(item);
+      this.adjustQuality(item);
+    });
     return this.items;
   }
+
+  adjustSellInDate(item) {
+    if (item.name === ItemNames.SULFURAS) {
+      return;
+    }
+    item.sellIn--;
+  }
+
+  adjustQuality(item) {
+    if (item.name === ItemNames.SULFURAS) {
+      return;
+    }
+    const adjustedQuality = item.quality + this.computeQualityAdjustment(item);
+    if (adjustedQuality >= 0 && adjustedQuality <= 50) {
+      item.quality = adjustedQuality;
+    }
+  }
+
+  computeQualityAdjustment(item) {
+    let adjustment = this.qualityChange;
+    if (this.isIncreasingValueItem(item)) {
+      adjustment = 1;
+    }
+    if (item.name === ItemNames.BACKSTAGE_PASS) {
+      // Backstage passes have their own unique rules.
+      if (this.isExpired(item)) {
+        // Backstage passes lose all value when expired.
+        adjustment = -item.quality;
+      } else if (item.sellIn <= 5) {
+        adjustment = adjustment * 3;
+      } else if (item.sellIn <= 10) {
+        adjustment = adjustment * 2;
+      }
+    } else if (this.isExpired(item) || this.isConjured(item)) {
+      // Expired items adjust their price by double the rate.
+      adjustment = adjustment * 2;
+    }
+    return adjustment;
+  }
+
+  isIncreasingValueItem(item) {
+    return (
+      item.name === ItemNames.AGED_BRIE ||
+      item.name === ItemNames.BACKSTAGE_PASS
+    );
+  }
+
+  isExpired(item) {
+    return item.sellIn < 0;
+  }
+
+  isConjured(item) {
+    return item.name.startsWith("Conjured");
+  }
 }
+
+const ItemNames = {
+  BACKSTAGE_PASS: "Backstage passes to a TAFKAL80ETC concert",
+  AGED_BRIE: "Aged Brie",
+  SULFURAS: "Sulfuras, Hand of Ragnaros",
+};
 
 module.exports = {
   Item,
-  Shop
-}
+  Shop,
+  ItemNames,
+};
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,92 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const {Shop, Item, ItemNames} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
   });
+
+  describe('UpdateQuality', () => {
+
+    let testItemName, testItemSellIn, testItemQuality, testItem, testShop;
+
+    beforeEach(()=> {
+      testItemName = 'foo';
+      testItemSellIn = 3;
+      testItemQuality = 40;
+      testItem = new Item(testItemName, testItemSellIn, testItemQuality);
+      testShop = new Shop([testItem]);
+    });
+
+    it('degrades quality by 1 as SellIn date approaches', () => {
+      testShop.updateQuality();
+      expect(testItem.quality).toEqual(testItemQuality - 1);
+    });
+
+    it('degrades quality twice as fast after the SellIn date passes', () => {
+      testItem.sellIn = -1;
+      testShop.updateQuality();
+      expect(testItem.quality).toEqual(testItemQuality - 2);
+    });
+
+    it('increases the quality of Aged Brie as it ages', () => {
+      const agedBrie = new Item(ItemNames.AGED_BRIE, 7, 10);
+      testShop.items.push(agedBrie);
+      testShop.updateQuality();
+      expect(agedBrie.quality).toEqual(11);
+    });
+
+    it('ensures item quality never exceeds 50', () => {
+      const agedBrie = new Item(ItemNames.AGED_BRIE, 7, 50);
+      testShop.items.push(agedBrie);
+      testShop.updateQuality();
+      expect(agedBrie.quality).toEqual(50);
+    });
+
+    it('does not modify the quality or sellIn date of Sulfuras', () => {
+      const sulfuras = new Item(ItemNames.SULFURAS, 7, 80);
+      testShop.items.push(sulfuras);
+      testShop.updateQuality();
+      expect(sulfuras.quality).toEqual(80);
+      expect(sulfuras.sellIn).toEqual(7);
+    });
+
+    it('increases the quality of backstage passes as they age', () => {
+      const backstagePass = new Item(ItemNames.BACKSTAGE_PASS, 20, 10);
+      testShop.items.push(backstagePass);
+      testShop.updateQuality();
+      expect(backstagePass.quality).toEqual(11);
+    });
+
+    it('increases quality of backstage passes by 2 when there are 10 days or less', () => {
+      const backstagePass = new Item(ItemNames.BACKSTAGE_PASS, 10, 10);
+      testShop.items.push(backstagePass);
+      testShop.updateQuality();
+      expect(backstagePass.quality).toEqual(12);
+    });
+
+    it('increases quality of backstage passes by 3 when there are 5 days or less', () => {
+      const backstagePass = new Item(ItemNames.BACKSTAGE_PASS, 5, 10);
+      testShop.items.push(backstagePass);
+      testShop.updateQuality();
+      expect(backstagePass.quality).toEqual(13);
+    });
+
+    it('decreases quality of backstage passes to 0 after sellIn date expires', () => {
+      const backstagePass = new Item(ItemNames.BACKSTAGE_PASS, -1, 10);
+      testShop.items.push(backstagePass);
+      testShop.updateQuality();
+      expect(backstagePass.quality).toEqual(0);
+    });
+
+    it('decreases quality of conjured items by double the normal rate', () => {
+      const conjuredItem = new Item("Conjured bread", 7, 10);
+      testShop.items.push(conjuredItem);
+      testShop.updateQuality();
+      expect(conjuredItem.quality).toEqual(8);
+    });
+
+  });
+
 });
